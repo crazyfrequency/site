022c68b45ce109c0fc832cde6da5c9b9b8343ceb
crazyfrequency <di.efimoff2015@gmail.com>
2022-11-06 15:08:17 +0400

    modified2

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
+const bp = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+var login = {"123":"123"},
+cookie=["123"];
 
 app.use('/resource', express.static('./site/'));
+app.use(cookieParser());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
-app.get("/*",async(request, response)=>{
-    console.log(request.path.slice(1))
+app.get("/main*",async(request, response)=>{
     response.sendFile("C:/Users/diefi/Desktop/site/site/pages/main.html")
 })
 
-app.listen(80,()=>{console.log(`Сервер запущен\nПорт:${80}`)});
+app.get("/api/login/check",async(request, response)=>{
+    console.log(request.cookies)
+    if(request.cookies?.key=="123")
+        response.sendStatus(200)
+    else response.sendStatus(403);
+})
+app.post("/api/login",async(request, response)=>{
+    console.log(request.body)
+    
+    response.cookie("key","123");
+    response.sendStatus(200);
+})
+app.get("*",async(request, response)=>{
+    response.redirect("/main")
+})
+
+app.listen(80,()=>{console.log(`Сервер запущен\nПорт:${80}`)}).on("error",async(e)=>{
+    console.error(e.name,"\n",e.message,"\n",e.stack)
+});
--- site/script.js
+++ site/script.js
@@ -1,7 +1,7 @@
+var allpages = [];
 async function main(){
     let theme = Number(localStorage.getItem("theme")),
     darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    console.log(theme)
     if(theme==1){
         document.body.classList.replace("dark-theme","light-theme");
         document.getElementsByClassName("theme-moon")[0].classList.remove("active");
@@ -18,9 +18,39 @@ async function main(){
             document.getElementsByClassName("theme-sun")[0].classList.remove("active");
         }
     }
+    for(let e of document.getElementsByClassName("list-group-item"))
+        allpages.push(e.dataset.target);
+    let path = window.location.pathname.replace(/\/main(\/|)/,"").replace("/","");
+    if(allpages.includes(path)){
+        for(let e of document.getElementsByClassName("list-group-item")){
+            e.classList.remove("active");
+        }
+        for(let e of document.getElementsByClassName("main-page-inner")){
+            e.classList.remove("active");
+        }
+        document.getElementById("b-"+path).classList.add("active");
+        document.getElementById(path).classList.add("active");
+    }else{
+        history.replaceState(null,null,"/main/send");
+    }
+    let res = await fetch("/api/login/check").catch(()=>null);
+    if(res.ok){
+        document.getElementById("login").classList.remove("active");
+    }
+    
 }
 window.onload=()=>main().catch(()=>null)
-
+window.onpopstate=()=>{
+    let path = window.location.pathname.replace(/\/main(\/|)/,"").replace("/","");
+    for(let e of document.getElementsByClassName("list-group-item")){
+        e.classList.remove("active");
+    }
+    for(let e of document.getElementsByClassName("main-page-inner")){
+        e.classList.remove("active");
+    }
+    document.getElementById("b-"+path).classList.add("active");
+    document.getElementById(path).classList.add("active");
+}
 async function embed_preview(){
     
     let prev1 = document.getElementsByClassName("embed-preview")[0],
@@ -34,10 +64,46 @@ async function embed_preview(){
     }
 }
 
-function menu(element){
+async function login(element){
+    element.classList.add("active");
+    let res = await fetch("/api/login",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+        login:login_text.value,
+        password:password_text.value,
+    })}).catch(()=>null);
+
+    element.classList.remove("active");
+    document.getElementById("login").classList.remove("active");
+}
+
+function menu(element,event){
+    if(event.ctrlKey) return window.open("../"+element.dataset.target, '_blank')
     for(let e of document.getElementsByClassName("list-group-item")){
         e.classList.remove("active");
     }
+    for(let e of document.getElementsByClassName("main-page-inner")){
+        e.classList.remove("active");
+    }
     element.classList.add("active");
+    document.getElementById(element.dataset.target).classList.add("active");
+    history.pushState(null,null,"../"+element.dataset.target)
+}
+
+function select_theme(element){
+    let theme_button1 = document.getElementsByClassName("theme-moon")[0].classList,
+    theme_button2 = document.getElementsByClassName("theme-sun")[0].classList;
+    if(element.classList.contains("theme-moon")){
+        theme_button1.add("active");
+        theme_button2.remove("active");
+        document.body.classList.replace("light-theme","dark-theme");
+        localStorage.setItem("theme",`${2}`);
+    }else{
+        theme_button2.add("active");
+        theme_button1.remove("active");
+        document.body.classList.replace("dark-theme","light-theme");
+        localStorage.setItem("theme",`${1}`);
+    }
 }
 
